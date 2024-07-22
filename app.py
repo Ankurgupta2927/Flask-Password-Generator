5622f690d616711f264ce0830625d09c4ba59049
Ankur Gupta <ankurgupta2729@gmail.com>
2024-07-22 10:56:38 +0000

    first

--- app.py
+++ app.py
@@ -1,7 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import random
+import string
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+def generate_password(length, uppercase, lowercase, digits, special):
+    char_set = ''
+    if uppercase:
+        char_set += string.ascii_uppercase
+    if lowercase:
+        char_set += string.ascii_lowercase
+    if digits:
+        char_set += string.digits
+    if special:
+        char_set += string.punctuation
+
+    if not char_set:
+        return ''
+
+    password = ''.join(random.choice(char_set) for _ in range(length))
+    return password
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    password = ''
+    if request.method == 'POST':
+        length = int(request.form['length'])
+        uppercase = 'uppercase' in request.form
+        lowercase = 'lowercase' in request.form
+        digits = 'digits' in request.form
+        special = 'special' in request.form
+
+        password = generate_password(length, uppercase, lowercase, digits, special)
+
+    return render_template('index.html', password=password)
+
+if __name__ == '__main__':
+    app.run(debug=True)
